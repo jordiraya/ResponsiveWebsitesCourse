e808f68d1bc35b49342c144b0c3b8ca2fa42d6da
Jordi Raya Lapuente <raya.jordi@gmail.com>
2018-05-15 18:17:49 +0200

    Lecture 76: Using PHP to make the form work

--- omnifood/resources/js/script.js
+++ omnifood/resources/js/script.js
@@ -1,7 +1,91 @@
 $(document).ready(function() {
 
-    $('h1').click(function() {
-        $(this).css('background-color', '#ff0000');
+    /* sticky navigation */
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px' /* will happen 60 px before section arribes at top */
     });
+
+    /* scroll on buttons */
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    }); 
+
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });     
+
+    /* navigation scroll from https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {                var target = $(this.hash);
+            target = target.length? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({scrollTop: target.offset().top}, 1000);
+                return false;
+            }
+        }
+    });
+
+    /* animations on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });    
+
+    /* mobile nav */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon');
+        
+        nav.slideToggle(200);        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }        
+    });
+
+    /* gmaps */
+    var map = new GMaps({
+        div: '.map',
+        lat: 41.3754675,
+        lng: 2.3,
+        zoom: 11
+    });
+
+    map.addMarker({
+        lat: 41.3754675,
+        lng: 2.1357329,
+        title: 'Mi casa',
+        infoWindow: {
+            content: '<p>My HQ</p>'
+        } 
+    });    
 
 });
